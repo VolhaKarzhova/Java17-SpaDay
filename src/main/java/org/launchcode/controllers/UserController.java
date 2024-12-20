0f239b8280bd976421ff6fabf4867e23a39394ad
olgakorzhova <olga1584624@gmail.com>
2024-12-19 20:22:11 -0600

    add studio

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -15,19 +17,13 @@ public class UserController {
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
-        }
-        else {
-            model.addAttribute("error", "Passwords do not match");
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors error, String verify) {
+        if(error.hasErrors() || !user.getPassword().equals(verify)){
+            model.addAttribute("error", "Something is not right with inputs");
             return "user/add";
         }
-
+        model.addAttribute(new User());
+        return "user/index";
     }
 
 
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,15 +1,29 @@
 package org.launchcode.models;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Username required")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters long")
     private String username;
+
+    @Email(message = "Invalid email try again")
     private String email;
+
+    @Min(value = 6, message = "Needs to be at least 6 characters long")
+    @NotBlank(message = "Password required")
     private String password;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+
+    public User(String username, @Nullable String email, String password) {
         this();
         this.username = username;
         this.email = email;
